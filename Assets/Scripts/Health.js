671d11545961241e570826e60acc52b882cc9e04
Dominik Ehrenberg <doe@crosscan.com>
2017-03-22 16:59:33 +0100

    Complete Game

--- Assets/Scripts/Health.js
+++ Assets/Scripts/Health.js
@@ -1,7 +1,13 @@
 ﻿#pragma strict
 
 public var health: float = 100.0f;
+public var HudHealth: UI.Slider;
 
 function RemoveHealth(amount: float) {
 	health = health - amount;
+	UpdateHUD();
+}
+
+function UpdateHUD() {
+	HudHealth.value = health;
 }
--- Assets/Scripts/Weapon.js
+++ Assets/Scripts/Weapon.js
@@ -1,8 +1,18 @@
 ﻿#pragma strict
 
+public var weaponSound: AudioClip;
+
+function Start() {
+	PlayWeaponSound();
+}
+
 function OnTriggerEnter2D(other: Collider2D) {
 	if (other.gameObject.tag == 'Enemy') {
 		Destroy(other.gameObject);
 		Destroy(gameObject);
 	}
+}
+
+function PlayWeaponSound() {
+	AudioSource.PlayClipAtPoint(weaponSound, Camera.main.transform.position);
 }
